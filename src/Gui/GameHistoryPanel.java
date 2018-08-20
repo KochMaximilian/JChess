ea6ee359353c06544d2613abdba34063737cdeab
Maximilian Koch <maximiliankochhoch@gmail.com>
2018-08-20 20:21:08 +0200

    update Game History Panel

--- src/Gui/GameHistoryPanel.java
+++ src/Gui/GameHistoryPanel.java
@@ -1,6 +1,7 @@
 package Gui;
 
 import Board.Board;
+import Board.Move;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -27,9 +28,37 @@ public class GameHistoryPanel extends JPanel {
         this.setVisible(true);
     }
 
-    void redo(final Board board, final MoveLog moveLog) {
+    void redo(final Board board, final MoveLog moveHistory) {
+        int currentRow = 0;
+        this.model.clear();
+        for(final Move move : moveHistory.getMoves()) {
+            final String moveText = move.toString();
+            if(move.getMovedPiece().getPieceAlliance().isWhite()) {
+                this.model.setValueAt(moveText, currentRow, 0);
+            } else if(move.getMovedPiece().getPieceAlliance().isBlack()) {
+                this.model.setValueAt(moveText, currentRow, 1);
+                currentRow++;
+            }
+        }
+        if(moveHistory.getMoves().size() > 0) {
+            final Move lastMove = moveHistory.getMoves().get(moveHistory.size() -1);
+            final String moveText = lastMove.toString();
 
+            if(lastMove.getMovedPiece().getPieceAlliance().isWhite()) {
+                this.model.setValueAt(moveText + calculateCheckAndCheckMateHash(board), currentRow -1, 1);
+            }
+        }
+        final JScrollBar vertical = scrollPane.getVerticalScrollBar();
+        vertical.setValue(vertical.getMaximum());
+    }
 
+    private String calculateCheckAndCheckMateHash(final Board board) {
+        if(board.currentPlayer().isInCheckMate()) {
+            return "#";
+        } else if(board.currentPlayer().isInCeck()){
+            return "+";
+        }
+        return "";
     }
 
     private static class DataModel extends DefaultTableModel {
@@ -38,6 +67,62 @@ public class GameHistoryPanel extends JPanel {
 
         DataModel() {
             this.values = new ArrayList<>();
+        }
+
+        public void clear() {
+            this.values.clear();
+            setRowCount(0);
+        }
+
+        @Override
+        public int getRowCount() {
+            if(this.values == null) {
+                return 0;
+            }
+            return this.values.size();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return NAMES.length;
+        }
+
+        @Override
+        public Object getValueAt(final int row, final int column) {
+            final Row currentRow = this.values.get(row);
+            if(column == 0 ) {
+                return currentRow.getWhiteMove();
+            } else if(column == 1) {
+                return currentRow.getBlackMove();
+            }
+            return null;
+        }
+
+        @Override
+        public void setValueAt(final Object aValue, final int row, final int column) {
+            final Row currentRow;
+            if(this.values.size() <= row){
+                currentRow = new Row();
+                this.values.add(currentRow);
+            } else {
+                currentRow = this.values.get(row);
+            }
+            if(column == 0) {
+                currentRow.setWhiteMove((String)aValue);
+            } else if(column == 1) {
+                currentRow.setBlackMove((String)aValue);
+                fireTableCellUpdated(row, column );
+            }
+        }
+
+        @Override
+        public Class<?> getColumnClass(final int column) {
+            return Move.class;
+        }
+
+        @Override
+        public String getColumnName(final int column) {
+            return NAMES[column];
         }
     }
 
